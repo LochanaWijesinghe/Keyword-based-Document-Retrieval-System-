b8cc95c6f2651b6b725fb4fd172ccc8f7ed738f5
Lochana Wijesinghe <samadhi.20230497@iit.ac.lk>
2025-05-15 10:10:38 +0530

    Setting up the database

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+//DB configuration
+
+const Pool = require("pg");
+require('dotenv').config();
+
+const pool= new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    post: process.env.DB_PORT,
+});
+
+module.exports = pool;
