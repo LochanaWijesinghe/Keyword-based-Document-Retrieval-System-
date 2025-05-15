7930cafebbeef26081092136b8affd10c6546f27
Lochana Wijesinghe <samadhi.20230497@iit.ac.lk>
2025-05-15 11:40:55 +0530

    Process document content

---
+++ backend/utils/textProcessor.js
@@ -0,0 +1,29 @@
+//process document content
+
+class TextProcess{
+
+    static stopWords = new Set(['the', 'is', 'at', 'which', 'on', 'a', 'an', 'and']);
+    //set is faster than array for checking if a word exists
+
+    //method to clean and split a string of text
+    static preprocess(text){
+        return text
+            .toLowerCase()
+            .replace(/[\w\s]/,'')   //clean the string of any characters that are not whitespaces, letters and digits
+            .split(/\s+/)   //splits the cleaned string into words
+            .filter(word => word && !TextProcessor.stopWords.has(word));  //remove empty string and stopwords
+    }
+
+    //to calculate the number of times each words(tokens) appears
+    static getTermFrequencies(tokens){
+        const frequencyMap = {};
+        
+        for(const token of tokens){
+            frequencyMap[token] = (frequencyMap[token] || 0)+ 1;
+        }
+        return frequencyMap;
+    }
+
+}
+
+module.exports = TextProcess;
